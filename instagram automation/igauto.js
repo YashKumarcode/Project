b3418d1c5447f5445079b8782a7776771455f41d
YashKumarcode <65227750+YashKumarcode@users.noreply.github.com>
2020-09-25 13:08:53 +0530

    Add files via upload

---
+++ instagram automation/igauto.js
@@ -0,0 +1,58 @@
+// npm i puppeteer   ---- to install puppeteer with chromium
+let puppeteer = require("puppeteer");
+let cFile = process.argv[2];
+let fs = require("fs");
+let pUrl = process.argv[3];
+// let nPost = process.argv[4];
+(async function () {
+  // browser create => icognito mode,fullscreen
+  try {
+      console.log(cFile);
+    let data = await fs.promises.readFile(cFile);
+    let { url, pwd, user } = JSON.parse(data);
+    // launch browser
+    let browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+      args: ["--start-maximized", "--disable-notifications"]
+    });
+    // tab
+    let tabs = await browser.pages();
+    let tab = tabs[0];
+    // dom => html 
+    //  browser=> 500ms request 
+    // hk login page
+    await tab.goto(url, { waitUntil: "networkidle2" });
+    // await tab.waitForSelector("input[type=text]");
+    await tab.type("input[type=text]", user, { delay: 120 });
+    await tab.type("input[type=password]", pwd, { delay: 120 });
+    //  _1xnd => group of post 
+    // _4-u2 _4-u8=> particular post
+    //  inside ._1xnd
+    // descendent => select 
+    // 1xnd => last
+    await Promise.all([
+      tab.click("button[type='submit']"), tab.waitForNavigation({
+        waitUntil: "networkidle2"
+      })
+    ])
+
+    await tab.click("button.sqdOP.yWX7d.y3zKF"), tab.waitForNavigation({
+        waitUntil : "networkidle0"
+    })
+  await tab.waitFor(2000);
+    await tab.type("input[placeholder='Search']", pUrl, { delay: 120 });
+    await tab.waitFor(2000);
+    await tab.keyboard.press('ArrowDown');
+    
+    await tab.keyboard.press("Enter");
+    
+    
+
+    
+  } catch (err) {
+    console.log(err)
+  }
+})()
+
+// node igauto.js "igcredential.json" "the_hindu"
